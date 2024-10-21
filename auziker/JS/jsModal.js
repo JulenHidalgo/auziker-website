6060cf2feba49a94bd75d1b8c9f2f7e976285fd6
JulenHidalgo <jhidalgo2377@gmail.com>
2024-10-21 22:06:54 +0200

    arreglo en apartado enfoque se abria mail

--- auziker/JS/jsModal.js
+++ auziker/JS/jsModal.js
@@ -6,7 +6,6 @@ var uso = document.getElementById("imgProductos");
 var productos = document.getElementById("modalProductos");
 
 btnEnfoque.onclick = function () {
-    window.location.href = "mailto:?to=pruebasemail741%40gmail.com&subject=Quiero%20formar%20parte%20siendo%20trabajador&body=Nombre:%0D%0ATel%C3%A9fono:%0D%0AEmail:%0D%0A%3FPor%20qu%C3%A9%20quieres%20unirte%20a%20nosotros%3F%3A%0D%0A%0D%0AAjunta%20tu%20CV%20debajo:";
     enfoque.style.display = "block";
     body.style.overflow = "hidden"; // Bloquear scroll del body
     icono.style.display = "none";
